81a2b4edfee56c5b4681c2822d0aae41d8952042
Agustin Ferreira <agustin@xivis.com>
2019-09-26 17:38:15 -0300

    POAP-43 DONE Remove drop table from update-schema.sql

--- db/update-schema.sql
+++ db/update-schema.sql
@@ -13,7 +13,7 @@ ALTER TABLE server_transactions
     ADD COLUMN status VARCHAR (100) NOT NULL default 'pending',
     ADD COLUMN gas_price VARCHAR (1000) NOT NULL;
 
-
+/* CREATE TABLE signers */
 CREATE TABLE signers (
   "id" SERIAL PRIMARY KEY,
   "signer" varchar(256) UNIQUE not null,
